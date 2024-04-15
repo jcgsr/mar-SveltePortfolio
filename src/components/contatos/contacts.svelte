9320c7255f8b203fca1b6f2e75e6c7fb4d287106
jcgsr <cabralguerra@gmail.com>
2024-04-15 20:31:37 -0300

    contatdos icons

--- src/components/contatos/contacts.svelte
+++ src/components/contatos/contacts.svelte
@@ -2,49 +2,39 @@
 	import Icon from '@iconify/svelte';
 </script>
 
-<section class="bg-slate-500">
-	<div>
-		<h1 class="text-center text-6xl mb-4">contatos</h1>
-		<div
-			class="rounded-lg overflow-hidden shadow-lg m-4 lg:w-1/4
-      hover:scale-105 delay-200 duration-700 dark:shadow-white"
-		>
-			<div class="flex flex-row">
-				<span>
-					<a href="https://wa.me/5579988284730?text=Gostaria%de%saber%o%valor%..." target="_blank">
-						<Icon
-							icon="logos:whatsapp-monochrome-icon"
-							inline={true}
-							style="font-size: 20px; font-weight: bold;"
-						/>
-					</a><span>
-						<span>
-							<a href="https://www.facebook.com/cabral8guerra" target="_blank">
-								<Icon icon="fa6-brands:square-facebook" />
-							</a><span>
-								<span>
-									<a href="https://www.instagram.com/cabralguerra/" target="_blank">
-										<Icon icon="simple-icons:instagram" />
-									</a><span>
-										<span>
-											<a href="mailto:cabralguerra@gmail.com" target="_blank">
-												<Icon icon="ic:outline-email" />
-											</a><span> </span></span
-										>
-									</span></span
-								>
-							</span></span
-						></span
-					></span
-				>
-			</div>
-		</div>
+<section class="bg-slate-100">
+	<h1 class="text-center text-6xl p-6 mb-4">contatos</h1>
+	<div
+		class="flex flex-col text-center justify-center m-auto
+    md:flex-row md:justify-around"
+	>
+		<a href="https://wa.me/5579988284730?text=Gostaria%de%saber%o%valor%..." target="_blank">
+			<Icon icon="mdi:whatsapp" />
+		</a>
+		<a href="https://www.facebook.com/cabral8guerra" target="_blank">
+			<Icon icon="mdi:facebook" />
+		</a>
+		<a href="https://www.instagram.com/cabralguerra/" target="_blank">
+			<Icon icon="mdi:instagram" />
+		</a>
+		<a href="mailto:cabralguerra@gmail.com" target="_blank">
+			<Icon icon="mdi:email" />
+		</a>
 	</div>
 </section>
 
 <style>
-	span {
-		margin: 0.3rem;
-		text-align: left;
+	a {
+		font-size: 8rem;
+		margin: 2rem auto;
+		transition: color ease-in-out 0.6s;
+	}
+	a:hover {
+		color: red;
+	}
+	@media only screen and (max-width: 820px) {
+		a {
+			font-size: 6rem;
+		}
 	}
 </style>
