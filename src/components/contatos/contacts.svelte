3f803ea66846df7cd4d1c3e2f9df9e81fb684ca1
jcgsr <cabralguerra@gmail.com>
2024-04-27 10:24:32 -0300

    css effect

--- src/components/contatos/contacts.svelte
+++ src/components/contatos/contacts.svelte
@@ -1,28 +1,55 @@
 <script>
 	import Icon from '@iconify/svelte';
+	let y;
 </script>
 
+<svelte:window bind:scrollY={y} />
 <section id="contatos" class="bg-gray-100 text-gray-500">
 	<h1 class="text-center text-5xl p-6 mb-4 font-kolnia uppercase">contatos</h1>
 	<div
 		class="flex flex-col text-center justify-center m-auto
     md:flex-row md:justify-around"
 	>
-		<a href="https://wa.me/5579988284730?text=Gostaria%de%saber%o%valor%..." target="_blank">
+		<a
+			href="https://wa.me/5579988284730?text=Gostaria%de%saber%o%valor%..."
+			class="aos-hidden-left"
+			class:aos-show={y >= 100}
+			target="_blank"
+		>
 			<Icon icon="mdi:whatsapp" />
 		</a>
-		<a href="https://github.com/jcgsr" target="_blank">
+		<a
+			href="https://github.com/jcgsr"
+			target="_blank"
+			class="aos-hidden-bottom"
+			class:aos-show={y >= 150}
+		>
 			<Icon icon="mdi:github" />
 		</a>
 
-		<a href="mailto:cabralguerra@gmail.com" target="_blank">
+		<a
+			href="mailto:cabralguerra@gmail.com"
+			target="_blank"
+			class="aos-hidden-bottom"
+			class:aos-show={y >= 150}
+		>
 			<Icon icon="mdi:email" />
 		</a>
 
-		<a href="https://www.facebook.com/cabral8guerra" target="_blank">
+		<a
+			href="https://www.facebook.com/cabral8guerra"
+			target="_blank"
+			class="aos-hidden-bottom"
+			class:aos-show={y >= 150}
+		>
 			<Icon icon="mdi:facebook" />
 		</a>
-		<a href="https://www.instagram.com/cabralguerra/" target="_blank">
+		<a
+			href="https://www.instagram.com/cabralguerra/"
+			target="_blank"
+			class="aos-hidden-right"
+			class:aos-show={y >= 100}
+		>
 			<Icon icon="mdi:instagram" />
 		</a>
 	</div>
@@ -42,5 +69,29 @@
 		a {
 			font-size: 4rem;
 		}
+	}
+	.aos-hidden-left {
+		opacity: 0;
+		filter: blur(3px);
+		transform: translateX(-100%);
+		transition: all 1.3s;
+	}
+	.aos-hidden-right {
+		opacity: 0;
+		filter: blur(3px);
+		transform: translateX(100%);
+		transition: all 1.3s;
+	}
+	.aos-hidden-bottom {
+		opacity: 0;
+		filter: blur(3px);
+		transform: translateY(100%);
+		transition: all 1.3s;
+	}
+
+	.aos-show {
+		opacity: 1;
+		filter: blur(0);
+		transform: translateX(0);
 	}
 </style>
--- src/routes/projetos/+page.svelte
+++ src/routes/projetos/+page.svelte
@@ -1,14 +1,17 @@
 <script>
 	import { projects } from './projects';
+	let y;
 </script>
 
+<svelte:window bind:scrollY={y} />
 <section class="bg-gray-700 mt-1">
 	<h1 class="text-center text-white font-orbitron uppercase text-5xl p-6 mb-4">projetos</h1>
 	<div class="flex flex-col lg:flex-row lg:flex-wrap justify-center justify-items-stretch">
 		{#each projects as { nome, img, tec1, tec2, tec3, tec4, end }}
 			<div
 				class="rounded-lg overflow-hidden shadow-md shadow-white m-4 lg:w-1/4
-        hover:scale-105 delay-200 duration-700 dark:shadow-white bg-gradient-to-r from-gray-400 to-gray-900 hover:from-gray-900 hover:to-gray-400 hover:delay-200"
+                hover:scale-105 delay-200 duration-700 dark:shadow-white bg-gradient-to-r from-gray-400 to-gray-900 hover:from-gray-900 hover:to-gray-400 hover:delay-200 aos-hidden-bottom"
+				class:aos-show={y >= 120}
 			>
 				<img src={img} alt={nome} />
 				<div class="flex flex-row">
@@ -34,5 +37,17 @@
 		color: white;
 		font-size: smaller;
 		border-radius: 5px;
+	}
+	.aos-hidden-bottom {
+		opacity: 0;
+		filter: blur(2px);
+		/*transform: translateY(100%);*/
+		transition: all 1.3s;
+	}
+
+	.aos-show {
+		opacity: 1;
+		filter: blur(0);
+		transform: translateX(0);
 	}
 </style>
