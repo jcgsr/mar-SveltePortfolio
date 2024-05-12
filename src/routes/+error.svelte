df2e7630bf5ce08d54e730cda9d6cc104cabdabe
jcgsr <cabralguerra@gmail.com>
2024-05-12 16:18:22 -0300

    error page font

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,13 +4,15 @@
 
 <div class="flex flex-col m-auto text-center justify-center align-top px-10 mt-14">
 	<section class="m-auto text-center">
-		<h1 class="text-8xl text-gray-800">404</h1>
+		<h1 class="text-8xl font-jura text-gray-800">404</h1>
 		<div class="my-4">
 			<Icon icon="mdi:error" width="240px" height="200px" style="color: #1f2937" />
 		</div>
-		<h3 class="text-2xl text-gray-800 my-4">Essa página não existe</h3>
-		<button class="outline my-2 p-3 rounded hover:bg-gray-800 hover:text-white duration-500">
-			<a href="/">Início</a>
+		<h3 class="text-2xl text-gray-800 font-jura my-4">Essa página não existe</h3>
+		<button
+			class="outline my-2 p-3 rounded font-jura font-bold hover:bg-gray-800 hover:text-white duration-500"
+		>
+			<a href="/">início</a>
 		</button>
 	</section>
 </div>
